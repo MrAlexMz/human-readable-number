6b0e1b082139788c656fdcbc5da8e7d9dc3be337
Alexander Mazalo <mralexmzs@gmail.com>
2021-08-02 14:09:21 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+    let numberText = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+    let result = '';
+    if (number === 0) {
+        return numberText[0];
+    }
+    if(number > 99) {
+        result += `${numberText[Math.floor(number/100)]} hundred`;
+    } 
+    if(number % 100 !== 0) {
+        if(number % 100 < 21 ) {
+            result += ` ${numberText[number % 100]}`;
+        } 
+        else {
+            result += ` ${numberText[Math.floor((number % 100) / 10) + 18]}`;
+            if(number % 10 != 0) {
+                result += ` ${numberText[number % 10]}`;
+            }
+        }
+    }
+    return result.trim();
 }
